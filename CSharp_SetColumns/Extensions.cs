7e4bb9ceaae003456b81bcf175c4c6b102cfdcc8
Zev Spitz <shivisi@hotmail.com>
2019-08-18 03:38:57 +0300

    Initial commit of SetColumns example in C#

---
+++ CSharp_SetColumns/Extensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using static Utils.Functions;
+using Utils;
+using System.Windows.Data;
+
+namespace CSharp_SetColumns {
+    public static class Extensions {
+        // We need the TResult parameter so the compiler can recognize the second parameter as an expression tree
+        public static void SetColumns<TSource, TResult>(this DataGrid dg, Expression<Func<TSource, TResult>> selector) {
+            dg.Columns.Clear();
+
+            // call parse fields
+            var fields = ParseFields(selector);
+
+            var prm = selector.Parameters[0];
+
+            var visitor = new XamlBindingPathVisitor();
+
+            // TODO validate expressions
+            // verify that each expression can be translated into a binding
+            // it has to be a MemberAccessExpression, of possibly multiple levels, returning a scalar value
+            // the innermost item in the MemberAccessExpression has to be the parameter of type TSource of the original expression
+            foreach (var (name,expr) in fields) {
+                if (!expr.Type.IsScalar()) {
+                    throw new InvalidOperationException("Expression's return type is non-bindable");
+                }
+
+                visitor.Visit(expr);
+                if (visitor.LastPath == null) {
+                    throw new InvalidOperationException("Unable to generate binding path from expression");
+                }
+
+                dg.Columns.Add(new DataGridTextColumn {
+                    Header = name,
+                    Binding = new Binding(visitor.LastPath)
+                });
+            }
+        }
+    }
+}
---
+++ CSharp_SetColumns/XamlBindingPathVisitor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_SetColumns {
+    public class XamlBindingPathVisitor : ExpressionVisitor {
+        private string currenPath;
+        private bool isBindablePath = true;
+
+        public string LastPath { get; private set; }
+
+        public override Expression Visit(Expression node) {
+            // https://stackoverflow.com/a/34794447/111794
+            bool firstVisit = currenPath == null;
+            if (firstVisit) {
+                currenPath= "";
+            }
+
+            isBindablePath = node is MemberExpression || node is LambdaExpression || node is ParameterExpression;
+
+            // https://stackoverflow.com/questions/36737463/stop-traversal-with-expressionvisitor
+            if (!isBindablePath && !firstVisit) { return node; }
+
+            var ret = base.Visit(node);
+
+            if (firstVisit) {
+                if (isBindablePath) {
+                    LastPath = currenPath.Substring(1);
+                } else {
+                    LastPath = null;
+                }
+                currenPath = null;
+                isBindablePath = true;
+            }
+            return ret;
+        }
+
+        protected override Expression VisitMember(MemberExpression node) {
+            var ret = base.VisitMember(node); ;
+            currenPath += "." + node.Member.Name;
+            return ret;
+        }
+
+        public override string ToString() => LastPath;
+    }
+}
